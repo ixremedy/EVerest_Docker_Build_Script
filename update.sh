90181c1053e86dcac2224b5b18682852cdcf1853
ixremedy <vinstance@gmail.com>
2024-01-19 22:18:27 +0200

    Added missing dependencies

--- update.sh
+++ update.sh
@@ -10,4 +10,5 @@ apt-get install -y cmake autoconf build-essential libboost-system-dev \
 libboost-thread-dev libboost-program-options-dev libboost-test-dev \
 libboost-filesystem-dev pkg-config libcap-dev openssl \
 libz-dev libssl-dev libcurl4-openssl-dev libexpat1-dev gettext gcc \
-python3 python3-pip default-jre openssh-client libboost-dev
+python3 python3-pip default-jre openssh-client libboost-dev \
+sqlite3 libsqlite3-dev libboost-log-dev nodejs npm git-
