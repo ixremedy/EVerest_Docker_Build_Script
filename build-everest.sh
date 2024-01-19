078a91845587bdbf6bdcfcf98f3d2cb89fa44be8
ixremedy <vinstance@gmail.com>
2024-01-19 23:57:44 +0200

    More pachages to install

--- build-everest.sh
+++ build-everest.sh
@@ -8,6 +8,8 @@ EVEREST_DEV=/home/$(whoami)/everest-dev
 export CPM_SOURCE_CACHE=$HOME/.cache/CPM
 export PATH=$PATH:/home/$(whoami)/.local/bin
 
+python3 -m pip install --upgrade pip setuptools wheel
+
 cd $EVEREST_DEV/everest-core
 mkdir build
 cd build
--- update.sh
+++ update.sh
@@ -8,7 +8,9 @@ apt update
 
 apt-get install -y cmake autoconf build-essential libboost-system-dev \
 libboost-thread-dev libboost-program-options-dev libboost-test-dev \
-libboost-filesystem-dev pkg-config libcap-dev openssl \
+libboost-filesystem-dev libboost-log-dev pkg-config libcap-dev openssl \
 libz-dev libssl-dev libcurl4-openssl-dev libexpat1-dev gettext gcc \
 python3 python3-pip default-jre openssh-client libboost-dev \
-sqlite3 libsqlite3-dev libboost-log-dev nodejs npm git-
+rsync wget doxygen graphviz clang-tidy cppcheck openjdk-17-jdk docker \
+docker-compose clang-format curl rfkill libpcap-dev libevent-dev \
+sqlite3 libsqlite3-dev nodejs npm git-
